16c18a4dda15f7c9fdaa23c26d2583b65c357956
KONDI SRINIVASU <srinivasu.kondi@gspann.com>
2024-10-23 15:48:12 +0530

    First commit2.4

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -12,11 +12,12 @@ const getPageTitle = async (url) => {
 
 const getAllPathsExceptCurrent = async (paths) => {
   const result = [];
+  // Remove first and last slash characters
   const pathsList = paths.replace(/^\/|\/$/g, '').split('/');
   for (let i = 0; i < pathsList.length - 1; i += 1) {
     const pathPart = pathsList[i];
     const prevPath = result[i - 1] ? result[i - 1].path : '';
-    const path = `${prevPath}/${pathPart}`.replace(/\/+/g, '/');
+    const path = `${prevPath}/${pathPart}`;
     const url = `${window.location.origin}${path}`;
     const name = await getPageTitle(url);
     if (name) {
@@ -33,12 +34,30 @@ const createLink = (path) => {
   return pathLink;
 };
 
+// Function to create an SVG for the Home icon
+const createHomeIcon = () => {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("viewBox", "0 0 24 24");
+  svg.setAttribute("width", "24");
+  svg.setAttribute("height", "24");
+  svg.setAttribute("fill", "currentColor");
+  svg.innerHTML = `
+    <path d="M12 3l10 9h-3v8h-6v-6h-4v6H5v-8H2l10-9z"/>
+  `;
+  return svg;
+};
+
 export default async function decorate(block) {
   const breadcrumb = createElement('nav', '', {
     'aria-label': 'Breadcrumb',
   });
   block.innerHTML = '';
-  const HomeLink = createLink({ path: '', name: 'Home', url: window.location.origin });
+
+  // Create the Home link with an SVG icon
+  const homeIcon = createHomeIcon();
+  const HomeLink = createLink({ path: '', name: '' });
+  HomeLink.prepend(homeIcon); // Add the SVG before the link text (if any)
+  HomeLink.append('Home'); // Optionally add text if you want it alongside the icon
   const breadcrumbLinks = [HomeLink.outerHTML];
 
   window.setTimeout(async () => {
