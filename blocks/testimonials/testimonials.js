a415246b048ca954cdf8b7f111a68c1c712909d8
aadigsp <aadi.ahlawat@gspann.com>
2025-03-17 11:24:13 +0530

    adding carousel logic

--- blocks/testimonials/testimonials.js
+++ blocks/testimonials/testimonials.js
@@ -1,20 +1,61 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
+function activateCarousel(action,currentIndex,testimonialsToShow)
+{
+  const testimonialsWrapper=document.querySelectorAll('ul');
+  const totalTestimonials=document.querySelectorAll('ul li').length;
+  if(action==='prev')
+  {
+    if(currentIndex>0)
+    {
+      currentIndex--;
+    }
+  }
+  if(action==='next')
+  {
+    if(currentIndex< totalTestimonials-testimonialsToShow)
+    {
+      currentIndex++;
+    }
+  }
+  updateTestimonials(testimonialsWrapper,currentIndex,testimonialsToShow);
+}
+
+function updateTestimonials(testimonialsWrapper,currentIndex,testimonialsToShow)
+{
+  const offset=-currentIndex*(100/testimonialsToShow)
+  testimonialsWrapper.style.transform=`translate(${offset})`;
+}
+
+
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   // create button wrapper and buttons for previous and next for textimonials carousel
   const buttonsWrapper = document.createElement('div');
   buttonsWrapper.classList.add('buttons');
+
+  let currentIndex=0;
+  const testimonialsToShow=4;
+
 // prev button
   const prevButton=document.createElement('button');
   prevButton.id="prev";
   prevButton.textContent="<";
+  prevButton.addEventListener('click',()=>
+  {
+    activateCarousel('prev',currentIndex,testimonialsToShow);
+  });
   //next button
   const nextButton=document.createElement('button');
   nextButton.id="next";
   nextButton.textContent=">";
+  nextButton.addEventListener('click',()=>
+    {
+      activateCarousel('next',currentIndex,testimonialsToShow);
+    });
+
   buttonsWrapper.appendChild(prevButton);
   buttonsWrapper.appendChild(nextButton);
 
@@ -33,6 +74,8 @@ export default function decorate(block) {
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
+
+
   block.textContent = '';
   block.append(ul);
   block.append(buttonsWrapper);
