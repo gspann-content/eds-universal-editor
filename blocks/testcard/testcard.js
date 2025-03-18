fd5e6dbd3b9b1f220beb68baf3dba511c420bfb8
Deepika Yadav <deepika.yadav@gspann.com>
2025-03-18 19:06:20 +0530

    adding feature bloc

--- blocks/testcard/testcard.js
+++ blocks/testcard/testcard.js
@@ -1,6 +1,36 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
-  const testCard = document.createElement('div');
-  testCard.className = 'test-card';
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+
+  // Create the text div
+  const textDiv = document.createElement('div');
+  textDiv.classList.add('text');
+  textDiv.innerText = 'Hello World'; // Set the text inside the div
+
+  // Append the text div to the overlay
+  overlay.appendChild(textDiv);
+
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    moveInstrumentation(row, li);
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
   block.textContent = '';
-  block.appendChild(testCard);
+  block.append(ul);
 }
