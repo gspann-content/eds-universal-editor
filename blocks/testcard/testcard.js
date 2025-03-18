8c1bae35d5df282f30bba93902e4a04d6e8fab1b
Deepika Yadav <deepika.yadav@gspann.com>
2025-03-18 14:39:17 +0530

    adding feature bloc

--- blocks/testcard/testcard.js
+++ blocks/testcard/testcard.js
@@ -1,27 +1,37 @@
 export default function decorate(block) {
-  const test = document.createElement('div');
-  test.classList.add('test');
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  // Create the image element
+
+  const container = block.querySelector('div');
+  const contentContainer = container.querySelector('div');
+  const image = contentContainer.querySelector('picture > img');
+  const img = document.createElement('img');
+  img.src = image.src;
+  img.alt = 'Image';
+  img.classList.add('card-image');
 
   // Create the content container
-  const testContent = document.createElement('div');
-  testContent.classList.add('test-content');
+  const cardContent = document.createElement('div');
+  cardContent.classList.add('card-content');
 
   // Create the title
   const title = document.createElement('h3');
-  title.textContent = 'test Title';
+  title.textContent = 'Card Title';
 
   // Create the paragraph
   const description = document.createElement('p');
-  description.textContent = 'This is a simple test with text and an image.';
+  description.textContent = 'This is a simple card with text and an image.';
 
   // Append the elements to the content container
-  testContent.appendChild(title);
-  testContent.appendChild(description);
+  cardContent.appendChild(title);
+  cardContent.appendChild(description);
 
-  // Append the image and content to the test
-  //   test.appendChild(testImage);
-  test.appendChild(testContent);
+  // Append the image and content to the card
+  card.appendChild(img);
+  card.appendChild(cardContent);
 
   block.textContent = '';
-  block.append(test);
+  block.append(card);
 }
