f1426b09f64f33ea7fd9dd33651b98505b3b65e5
aadigsp <aadi.ahlawat@gspann.com>
2025-03-18 12:55:57 +0530

    adding featured-products with lint

--- blocks/testimonials/testimonials.js
+++ blocks/testimonials/testimonials.js
@@ -1,57 +1,49 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
-let currentIndex=0;
-const testimonialsToShow=3;
-function activateCarousel(action,testimonialsToShow)
-{
-  const testimonialsWrapper=document.querySelector('.testimonials ul');
-  const totalTestimonials=testimonialsWrapper.querySelectorAll('li').length;
-  if(action==='prev')
-  {
-    if(currentIndex>0)
-    {
-      currentIndex--;
-    }
-  }
-  if(action==='next')
-  {
-    if(currentIndex < totalTestimonials-testimonialsToShow)
-    {
-      currentIndex++;
-    }
-  }
-  updateTestimonials(testimonialsWrapper,currentIndex,testimonialsToShow);
+function updateTestimonials(testimonialsWrapper, currentIndex, testimonialsToShow) {
+  const offset = -currentIndex * (100 / testimonialsToShow);
+  testimonialsWrapper.style.transform = `translate(${offset}%)`;
 }
-function updateTestimonials(testimonialsWrapper,currentIndex,testimonialsToShow)
-{
-  const offset=-currentIndex*(100/testimonialsToShow)
-  testimonialsWrapper.style.transform=`translate(${offset}%)`;
+
+function activateCarousel(action, testimonialsToShow) {
+  let currentIndex = 0;
+  const testimonialsWrapper = document.querySelector('.testimonials ul');
+  const totalTestimonials = testimonialsWrapper.querySelectorAll('li').length;
+  if (action === 'prev') {
+    if (currentIndex > 0) {
+      currentIndex -= 1;
+    }
+  }
+  if (action === 'next') {
+    if (currentIndex < totalTestimonials - testimonialsToShow) {
+      currentIndex += 1;
+    }
+  }
+  updateTestimonials(testimonialsWrapper, currentIndex, testimonialsToShow);
 }
 
 export default function decorate(block) {
-  
+  const testimonialsToShow = 3;
   /* change to ul, li */
   const ul = document.createElement('ul');
   // create button wrapper and buttons for previous and next for textimonials carousel
   const buttonsWrapper = document.createElement('div');
   buttonsWrapper.classList.add('buttons');
-// prev button
-  const prevButton=document.createElement('button');
-  prevButton.id="prev";
-  prevButton.textContent="<";
-  prevButton.addEventListener('click',()=>
-  {
-    activateCarousel('prev',testimonialsToShow);
+  // prev button
+  const prevButton = document.createElement('button');
+  prevButton.id = 'prev';
+  prevButton.textContent = '<';
+  prevButton.addEventListener('click', () => {
+    activateCarousel('prev', testimonialsToShow);
   });
-  //next button
-  const nextButton=document.createElement('button');
-  nextButton.id="next";
-  nextButton.textContent=">";
-  nextButton.addEventListener('click',()=>
-    {
-      activateCarousel('next',testimonialsToShow);
-    });
+  // next button
+  const nextButton = document.createElement('button');
+  nextButton.id = 'next';
+  nextButton.textContent = '>';
+  nextButton.addEventListener('click', () => {
+    activateCarousel('next', testimonialsToShow);
+  });
 
   buttonsWrapper.appendChild(prevButton);
   buttonsWrapper.appendChild(nextButton);
