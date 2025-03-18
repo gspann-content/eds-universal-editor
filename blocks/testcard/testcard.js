b56f0906b9647c6841b5a3d686ec6e0da6bb69e3
Deepika Yadav <deepika.yadav@gspann.com>
2025-03-18 18:19:57 +0530

    adding feature bloc

--- blocks/testcard/testcard.js
+++ blocks/testcard/testcard.js
@@ -1,6 +1,3 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
- console.log(block)
+  console.log(block);
 }
