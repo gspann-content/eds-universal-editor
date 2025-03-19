560bc757b9cfd52592cca14367cb0874ee6c34b3
Deepika Yadav <deepika.yadav@gspann.com>
2025-03-19 21:31:06 +0530

    adding feature bloc

--- blocks/products/products.js
+++ blocks/products/products.js
@@ -1,16 +1,56 @@
-async function getP() {
+async function fetchData(url) {
   try {
-    const response = await fetch('https://fakestoreapi.com/products');
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-    } else {
-      throw new Error('Failed to fetch data');
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error getting data: ${response.status}`);
     }
+    return await response.json();
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Fetch error:', error);
+    return [];
   }
 }
-export default function decorate() {
-  getP();
+
+export default function decorate(block) {
+  const container = document.createElement('div');
+  container.className = 'container';
+
+  fetchData('https://fakestoreapi.com/products').then((data) => {
+    data.forEach((item) => {
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      const img = document.createElement('img');
+      img.src = `${item.image}`;
+      img.alt = 'User image';
+      img.className = 'card-image';
+
+      const cardTitle = document.createElement('h1');
+      cardTitle.textContent = item.category;
+      cardTitle.className = 'card-title';
+
+      const cardText = document.createElement('p');
+      cardText.textContent = item.description;
+      cardText.className = 'card-text';
+
+      const cardPrice = document.createElement('p');
+      cardPrice.textContent = item.price;
+      cardPrice.className = 'card-text';
+
+      // const button = document.createElement('button');
+      // button.textContent = 'View Profile';
+      // button.className = 'card-button';
+
+      // Append elements
+      card.appendChild(img);
+      card.appendChild(cardTitle);
+      card.appendChild(cardText);
+      card.appendChild(cardPrice);
+      container.appendChild(card);
+    });
+
+    block.appendChild(container);
+  }).catch((error) => {
+    console.error('Error creating cards:', error);
+  });
 }
