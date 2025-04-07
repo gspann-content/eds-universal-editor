2cbd837697e7f9d4e6b2274b7fa3616f3232b8af
nehadwivedi38 <dwivedineha083@gmail.com>
2025-04-07 06:33:32 +0530

    fixed build issues

--- blocks/pagging/pagging.js
+++ blocks/pagging/pagging.js
@@ -4,21 +4,16 @@ import {
 
   export default function decorate(block) {
     const testpageDiv = div({class: 'pagging'});
-
-
     const title = block.querySelector('h1');
     const text = block.querySelector('p');
-
     if(title) {
         text.classList.add('pagging-title');
         testpageDiv.append(text);
     }
-
     if(text) {
         text.classList.add('pagging-text');
         testpageDiv.append(text);
     }
-
     block.innerHTML = '';  //clear the existing content 
     block.append(testpage);
   }
--- blocks/testpage/testpage.js
+++ blocks/testpage/testpage.js
@@ -4,16 +4,13 @@ import {
 
   export default function decorate(block) {
     const testpageDiv = div({class: 'testpage'});
-
     const image = block.querySelector('img');
-
     if(image) {
         image.classList.add('testpage-image');
         testpageDiv.append(image);
     }
 
     const text = block.querySelector('h1');
-
     if(text) {
         testpageDiv.append(text);
     }
