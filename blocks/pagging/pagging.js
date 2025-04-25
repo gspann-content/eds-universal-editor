7dc14510e3a071182e60e0c7b344a60b215867cf
nehadwivedi38 <dwivedineha083@gmail.com>
2025-04-25 12:14:42 +0530

    add new block items-cards

--- blocks/pagging/pagging.js
+++ blocks/pagging/pagging.js
@@ -1,63 +1,104 @@
 import {
-    h1, p, div, ul, li,h3
+    h1, p, div, ul, li,h3,h6,button
   } from '../../scripts/dom-builder.js';
 
    export default  async function decorate(block) {
-    const testpageDiv = div({class: 'global-text-center'});
+    const testpageDiv = div({class:' mainContainer'});
 
     const title = block.querySelector('h1');
     const text = block.querySelector('p');
-  
+
+    const titleDiv = div({class: 'titleContainer '});
+    const textDiv = div({class: 'textContainer '});
 
     if(title) {
-        title.className = 'global-text-2xl';
-        testpageDiv.append(title);
+        title.classList.add = 'titleCarousel ';
+        titleDiv.append(title);
     }
 
     if(text) {
-        text.className = 'text-center';
-        testpageDiv.append(text);
+        text.classList.add = 'textCarousel';
+        textDiv.append(text);
     }
 
+    testpageDiv.append(titleDiv,textDiv);
     block.append(testpageDiv);
 
-    const cardContainer = div ({class :' global-bg-grey-100 global-flex global-flex-wrap global-gap-x-[25px] global-p-4 global-border global-border-[#ccc] '});
-
-    try {
-     const res = await fetch('https://dummyjson.com/products'); //to fetch data 
-      const data =  await res.json(); 
-      console.log(data);
-
-      data.products.forEach(product => {    // to access each data 
-        const productDiv = div({class :'global-w-3/12 global-gap-x[25px] global-gap-4'});
-      
-
-        const productTitle = document.createElement('h2');
-        productTitle.textContent = product.title;
-        productTitle.className = 'font-bold text-xl';
-
-        const productDesc = document.createElement('p');
-        productDesc.textContent = product.description;
-        productDesc.className = "font-normal text-center border-2";
-
-        const productCategory = document.createElement('h6');
-        productCategory.textContent = product.category;
-        productCategory.className = "font-normal text-center border-2";
-
-        // const productImage = document.createElement('img');
-        // productImage.src = imageUrl;
 
 
-        productDiv.append(productTitle,productDesc) ;
-        cardContainer.append(productDiv);
-        testpageDiv.append(cardContainer);
+    const res = await fetch('https://dummyjson.com/products');
+    const data = await res.json();
 
-      });
+    const carouselWrapper = document.createElement('div');
+    carouselWrapper.className = 'carousel-wrapper';
 
-    } catch (error) {
-      console.error('error');
-    }
+    const cardTrack = document.createElement('div');
+    cardTrack.className = 'carousel-track';
 
-    block.innerHTML = '';
-    block.append(testpageDiv);
+    data.products.forEach(product => {
+      const card = document.createElement('div');
+      card.className = 'carousel-card';
+
+      const title = document.createElement('h2');
+        title.textContent = product.title;
+
+        const desc = document.createElement('p');
+        desc.textContent = product.description;
+
+        const btn = document.createElement('button');
+        btn.innerHTML = "View Details";
+        btn.classList.add('button');
+
+        card.append(title,desc,btn);
+        cardTrack.appendChild(card);
+    });
+
+    const cardHeadDiv = document.createElement('div');
+    cardHeadDiv.classList.add('card-head-div');
+
+    const heading = document.createElement('h5');
+    heading.classList.add('cardHeading');
+    heading.innerHTML = "Top Selling Products";
+
+    const browsebtn = document.createElement('button');
+    browsebtn.innerHTML = "Browse 200 Products ->";
+    browsebtn.classList.add('browse-button');
+
+
+    const arrows = div({class: 'carousel-arrows'});
+
+    const leftBtn = button({class: ' slide-button' , id: 'carousel-left-btn'});
+
+    const leftIcon = document.createElement('img');
+    leftIcon.src = '/icons/left-icon.svg';
+    leftIcon.classList.add('left-icon');
+    leftBtn.appendChild(leftIcon);
+
+
+    const rightBtn = button({class: 'slide-button', id: 'carousel-right-btn'});
+
+    const rightIcon = document.createElement('img');
+    rightIcon.src = '/icons/right-icon.svg';
+    rightIcon.classList.add('right-icon');
+    rightBtn.appendChild(rightIcon);
+
+   arrows.appendChild(leftBtn);
+   arrows.appendChild(rightBtn);
+
+   cardHeadDiv.append(heading, browsebtn);
+   cardHeadDiv.append(arrows);
+   carouselWrapper.append(cardHeadDiv, cardTrack);
+
+   const scrollAmount = 300;
+
+   leftBtn.addEventListener('click', () => {
+    cardTrack.scrollLeft -= scrollAmount;
+
+   }); 
+   rightBtn.addEventListener('click', () => {
+    cardTrack.scrollLeft += scrollAmount;
+   });
+
+   block.appendChild(carouselWrapper);
+
   }
